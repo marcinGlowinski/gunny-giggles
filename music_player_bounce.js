137b14551ec28e9157a0fdc9f80e443a22d8edca
Marcin Głowiński <marcglowinski@gmail.com>
2025-09-06 05:59:52 +0200

    fix html, small changes to music_player_bounce.js

--- music_player_bounce.js
+++ music_player_bounce.js
@@ -4,26 +4,18 @@ document.addEventListener("kitysLoaded", () => {
     const header = document.getElementById("header");
     const kittys = document.getElementsByClassName("kity");
 
-    if (!kittys.length) {
-        console.log("Could not find elements with class name kity");
-        return;
-    }
-
     player.bind(SC.Widget.Events.PLAY, () => {
         iframe.classList.add("bounce");
         header.className = "bounce";
         Array.from(kittys).forEach(element => element.classList.add("bounce"));
     });
 
-    player.bind(SC.Widget.Events.PAUSE, () => {
+    const removeBounce = () => {
         iframe.classList.remove("bounce");
         header.className = "";
         Array.from(kittys).forEach(element => element.classList.remove("bounce"));
-    });
+    }
 
-    player.bind(SC.Widget.Events.FINISH, () => {
-        iframe.classList.remove("bounce");
-        header.className = "";
-        Array.from(kittys).forEach(element => element.classList.remove("bounce"));
-    });
+    player.bind(SC.Widget.Events.PAUSE, () => removeBounce())
+    player.bind(SC.Widget.Events.FINISH, () => removeBounce());
 });
